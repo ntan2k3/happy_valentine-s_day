9acc17b913fa727ef795ab39aa9cf9eabd423264
Nguyễn Trường An <90670565+ntan2k3@users.noreply.github.com>
2023-03-12 09:49:10 +0700

    Update app.js
    Signed-off-by: Nguyễn Trường An <90670565+ntan2k3@users.noreply.github.com>

--- app.js
+++ app.js
@@ -5,3 +5,22 @@ btn.addEventListener('click', () => {
     song.play();
     wish.style.visibility = 'visible';
 });
+
+function addHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+
+    heart.innerText = '🤍';
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+setInterval(addHeart, 300);
